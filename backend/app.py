1631a0cf0db37c095ec3221b56d145bbcea74096
RUCHITHA <ruchuruchib@gmail.com>
2026-03-01 22:25:34 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,16 @@
 import requests
+import os
 
-API_KEY = "YOUR_API_KEY"
+API_KEY = os.getenv("61d8bc3f9918e72570cc542bf92c2089")
 
-def get_weather(city):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-    res = requests.get(url).json()
+def get_weather(city: str):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    data = response.json()
 
     return {
-        "temperature": res["main"]["temp"],
-        "humidity": res["main"]["humidity"],
-        "wind_speed": res["wind"]["speed"],
+        "temperature": data["main"]["temp"],
+        "humidity": data["main"]["humidity"],
+        "wind_speed": data["wind"]["speed"],
         "uv_index": 5
     }
