7c71906dd30292066444e2e3013921fe1c281f73
RUCHITHA <ruchuruchib@gmail.com>
2026-03-03 23:23:32 +0530

    backend

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,47 @@
-import requests
+from fastapi import FastAPI, HTTPException
+import numpy as np
+import joblib
+from backend.utils import get_weather
 
-API_KEY = "ff06a7b694ad491fbed180621260203"
+app = FastAPI()
 
-def get_weather(city: str):
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-    response = requests.get(url)
-    data = response.json()
-    
-    if response.status_code != 200:
-        raise Exception(data.get("message", "API Error"))
+# Load model
+model = joblib.load("model/heat_stress_model.pkl")
 
-    return {
-        "temperature": data["main"]["temp"],
-        "humidity": data["main"]["humidity"],
-        "wind_speed": data["wind"]["speed"],
-        "uv_index": 5
-    }
+@app.get("/")
+def home():
+    return {"message": "AI Urban Heat Stress Predictor API Running"}
+
+@app.post("/predict")
+def predict(data: dict):
+    try:
+        weather = get_weather(data["city"])
+
+        features = np.array([[
+            weather["temperature"],
+            weather["humidity"],
+            weather["wind_speed"],
+            weather["uv_index"],
+            data["age"],
+            data["working_hours"],
+            data["hydration_level"]
+        ]])
+
+        risk = float(model.predict(features)[0])
+
+        if risk < 25:
+            category = "Safe"
+        elif risk < 50:
+            category = "Moderate"
+        elif risk < 75:
+            category = "High"
+        else:
+            category = "Critical"
+
+        return {
+            "risk_score": round(risk, 2),
+            "category": category
+        }
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
