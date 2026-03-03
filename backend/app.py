ee8f798c1c8bff26e81edf4375fe5e9f35234c69
RUCHITHA <ruchuruchib@gmail.com>
2026-03-03 23:06:26 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ def get_weather(city: str):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     response = requests.get(url)
     data = response.json()
-
+    
     if response.status_code != 200:
         raise Exception(data.get("message", "API Error"))
 
